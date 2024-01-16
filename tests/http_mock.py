c778fe9c33daa176438f69d1ca5678d1bd2cc588
Michael-Zapivahin <Zapivahin@ya.ru>
2024-01-16 15:17:24 +0300

    add test into prod

--- tests/http_mock.py
+++ tests/http_mock.py
@@ -13,5 +13,10 @@ async def give_response(endpoint_method: str):
     return True
 
 
+@app.get('/get_test')
+async def get_test():
+    pass
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5001)
